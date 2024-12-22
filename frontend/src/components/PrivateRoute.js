99709bbbdc02f1d616f0554b6b7935664348abbc
Alexander <tverdokhlebalex@me.com>
2024-12-22 22:43:45 +0400

    Временное отключение авторизации для тестирования

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,21 +1,8 @@
-import React, { useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
-
-import { AuthContext } from "../contexts/AuthContext";
+import React from "react";
 
 function PrivateRoute({ children }) {
-  const { authToken } = useContext(AuthContext); // Получаем токен из контекста
-  const location = useLocation(); // Хук для получения текущего пути
-
-  // Если пользователь не авторизован, перенаправляем на страницу логина,
-  // сохраняя текущий маршрут для дальнейшего перенаправления
-  if (!authToken) {
-    return <Navigate to="/" state={{ from: location }} replace />;
-  }
-
-  // Если пользователь авторизован, отображаем защищенные компоненты
+  // Временно убрана проверка токена, чтобы пропускать всех пользователей
   return children;
 }
 
-// Экспорт компонента по умолчанию
 export default PrivateRoute;
